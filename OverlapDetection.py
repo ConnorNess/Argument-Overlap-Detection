cffe6cb381639784b15df178d850d6f5e7b3527e
Connor Ness <35226859+ConnorNess@users.noreply.github.com>
2021-07-28 21:55:48 +0100

    multiple overlap check

--- OverlapDetection.py
+++ OverlapDetection.py
@@ -13,6 +13,16 @@ import numpy as np
 #Check against hand detected overlaps
 #Return these
 
+################################################################################################################################
+#If something is detected by all algorithms, might be worth noting what that is
+
+def compareLists(multi_overlaps, overlapsA, overlapsB, overlapsC):
+    
+    overlaps = itertools.product(overlapsA, overlapsB, overlapsC)
+
+    for a, b, c in overlaps:
+        if a == b == c:
+            multi_overlaps.append(a)
 
 ################################################################################################################################
 #Compare detected overlaps to self detected overlaps
@@ -21,8 +31,6 @@ def compareOverlaps(handoverlaps, overlaps):
     
     matches = []
     matches_count = 0
-    matches_missed = []
-    matches_missed_count = 0
     matches_false = []
     matches_false_count = 0
 
@@ -95,7 +103,7 @@ def hamming(token1, token2):
     for char in range (len(token2)): #loop for the shortest word
         if token1[char] != token2[char]:
             distance += 1
-    distance += (len(token2) - len(token1)) #Add the remaining length of the input
+    distance += (len(token1) - len(token2)) #Add the remaining length of the input
     return distance
 
 def jaro(token1, token2):
@@ -142,8 +150,8 @@ def getSADFaces(arguments):
     #Turn each json file into an array of strings holding the atoms + their ID
 
     domainpath = (os.path.dirname(os.path.realpath(__file__)) + "\\Pets\\SADFace\\Hand Done\\")
-    SADFaces = os.listdir(domainpath)
-    #SADFaces = ['2229.json', '2235.json'] #Used for testing quickly
+    #SADFaces = os.listdir(domainpath)
+    SADFaces = ['2229.json', '2235.json'] #Used for testing quickly
 
     for each in SADFaces:
             with open(domainpath + each) as SADFace:
@@ -200,7 +208,7 @@ for a, b in itertools.combinations(arguments, 2):
     thislev = []
     #levenshtein
     levdist = levenshtein(a[0],b[0]) #0 = the text, 1 = the id
-    if (levdist <= 1): #ACCEPTABLE OVERLAP HERE - gotten through testing
+    if (levdist <= 10): #ACCEPTABLE OVERLAP HERE - gotten through testing
         thislev.append(a[1])
         thislev.append(b[1])
         levoverlaps.append(thislev)
@@ -211,7 +219,7 @@ for a, b in itertools.combinations(arguments, 2):
         hamdist = hamming(b[0], a[0])
     else:
         hamdist = hamming(a[0], b[0])
-    if(hamdist <= 5): #ACCEPTABLE OVERLAP HERE
+    if(hamdist <= 20): #ACCEPTABLE OVERLAP HERE
         thisham.append(a[1])
         thisham.append(b[1])
         hamoverlaps.append(thisham)
@@ -219,7 +227,7 @@ for a, b in itertools.combinations(arguments, 2):
     thisjaro = []
     #jaro
     jaroratio = jaro(a[0], b[0])
-    if (jaroratio >= 0.95): #ACCEPTABLE OVERLAP HERE
+    if (jaroratio >= 0.7): #ACCEPTABLE OVERLAP HERE
         thisjaro.append(a[1])
         thisjaro.append(b[1])
         jarooverlaps.append(thisjaro)
@@ -235,3 +243,8 @@ print("jaro")
 compareOverlaps(handoverlaps, jarooverlaps)
 print("____________________________")
 #Total number of overlaps = every node in txt minus how many lines
+
+
+multi_overlaps = []
+compareLists(multi_overlaps, levoverlaps, hamoverlaps, jarooverlaps)
+print(multi_overlaps)
